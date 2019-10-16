4d16662775b982c87101c36372671368a82f5130
keith120314 <56246249+keith120314@users.noreply.github.com>
2019-10-16 11:05:36 +0800

    Add files via upload

---
+++ backup-about-kogakusha/about-kogakusha.php
@@ -0,0 +1,230 @@
+<?php include 'header.php'; ?>
+
+
+<div class="theBody page">
+	<div class="resizer" id="resizer">
+
+		<div class="container">
+		
+		
+
+			<div class="section section-page group">
+				<div class="page-image">
+				<img src="images/main-about.png"/>
+				</div>
+				<div class="content fontA group">
+					<div class="main-caption group">
+						<h1>府中校</h1>
+					</div>
+				</div>
+			</div>		
+		
+			<div class="section breadcrumbs">
+					<div class="content">TOP ＞ 東京パソコンアカデミーとは</div>
+			</div>
+			<div class="section content-menu">
+					<div class="content">
+						<a class="toggle">ページ内MENU</a>
+						<ul>
+								<li><a href="">東京パソコンアカデミーの７つの特長</a>
+								</li><li><a href="">東京パソコンアカデミーの実績</a></li>
+						</ul>							
+					</div>
+			</div>
+
+			<div class="section group spacer">
+				<div class="content fontA group rtitle">
+					<p>東京パソコンアカデミーは、地域密着のパソコン教室として開校32年を迎えました。開講当初は「東京ワープロアカデミー」という名前でスタートをし、時代の流れとともに「東京パソコンアカデミー」と名前を変え、地域の皆様のパソコンスキル向上への貢献に力を入れてきました。私たちは以下の理念を掲げています。</p>
+					<h2>『私たちは生徒の幸せのためにICTスキルの習得を通じ、「生きる力」を育みます』</h2>
+					<div class="grid grid-2x">
+						<div>							
+							<p>この「生きる力」とは、「現代社会を生き抜く力」を表しています。コンピュータを扱うスキルは、現代社会において必須のスキルとなります。お仕事や学校、地域社会などの現場で役立つ、即戦力となるスキルの習得を目指し、受講者の目的に合わせた体系的なカリキュラムを構築し、密度の高い授業を行います。</p>
+							<p>東京パソコンアカデミーは授業や職業訓練を通して、皆様の「現代社会を生き抜くために必要な力」を身に付けるお手伝いをしたいと考えております。また、我々が最も大切にしているのは受講生とのコミュニケーション。講師は上級資格を取得しており、分かりやすさ、親切さは地域No.1だと自負しております。</p>
+							<p>受講生が気楽にいつでも質問できる雰囲気作りと、「東京パソコンアカデミーで学んでよかった！」と思ってもらえるようにスキル向上に努め、生徒の皆様が楽しく笑顔で通える教室を目指し、日々励んでおります。</p>
+						</div><div
+						><img src="images/about.png" /></div>
+					</div>
+				<div class="section content group-1">
+				<p>東京パソコンアカデミーでは、パソコン教室、職業訓練の他に、<strong>試験会場、企業向けパソコン研修、講師派遣</strong>なども運営をしております。<strong>団体向け試験</strong>も行っておりますので、お申込み、お問い合わせは右記お問い合わせフォームよりお気軽にお待ちしております。</p>
+				<a href="#" class="blue-1">法人向けのお問い合わせ</a>
+				</div>
+			</div>
+			<div class="video-1">
+			<h1>動画でレッスンの様子をご覧いただくことができます！</h1>
+			<img src="images/スクリーンショット 2019-08-05 8.44.09.png">
+			</div>
+		</div>
+
+			
+			<div class="section section03 group spacer">
+				<div class="content content03 fontA group">
+					<div class="ptitle"><h2>東京パソコンアカデミーの<span>７つの特長</span></h2></div>								
+						
+					<div class="grid grid-2x spacer counter">
+							<div class="box-shadow feature">
+								<div class="grid grid-2x">
+									<div>
+										<h3>オリジナルテキストでわかりやすい!!</h3>
+										<p>インストラクターのこれまでの経験を生かした、わかりやすく学びやすいテキストです。</p>
+									</div><div>
+									<img src="images/about01.png" /></div>
+								</div>
+							</div><div class="box-shadow feature">
+								<div class="grid grid-2x">
+									<div>
+										<h3>インストラクターは全員MOSマスター取得!!</h3>
+										<p>上級レベルの資格を取得し、どんなご質問にもお答えできるよう、日々インストラクターも勉強しています。</p>
+									</div><div>
+									<img src="images/about02.png" /></div>
+								</div>
+							</div><div class="box-shadow feature">
+								<div class="grid grid-2x">
+									<div>
+										<h3>受講料1時間1,500円（税抜）で通いやすい!!</h3>
+										<p>受講料1時間1,500円(入会金5,000円)。月謝制です。短期間で学ぶ通い放題（月謝30,000円）も人気です。</p>
+										<p>※価格は税抜価格です。</p>
+										<a class="arrow-link floatL" href="#">個人レッスンはこちら</a>
+									</div><div>
+									<img src="images/about03.png" /></div>
+								</div>
+							</div><div class="box-shadow feature">
+								<div class="grid grid-2x">
+									<div>
+										<h3>資格取得に強い!!</h3>
+										<p>試験数は全国9位。MOSテストセンター特別賞、OdysseyCBY賞を受賞など数々の試験会場として受賞を受けています。試験合格率は100%！実績があるからこそ、対策も細かく行います。</p>
+										<a class="arrow-link floatL" href="#">受験できる資格こちら</a>
+									</div><div>
+									<img src="images/about04.png" /></div>
+								</div>
+							</div><div class="box-shadow feature">
+								<div class="grid grid-2x">
+									<div>
+										<h3>職業訓練校なので就職・転職に強くなれる!!</h3>
+										<p>平成22年度より国からの委託を受けて求職者に向けた「職業訓練」を行っております。</p>
+										<a class="arrow-link floatL" href="#">個人レッスンはこちら</a>
+									</div><div>
+									<img src="images/about05.png" /></div>
+								</div>
+							</div><div class="box-shadow feature">
+								<div class="grid grid-2x">
+									<div>
+										<h3>企業へ向けた講師派遣も満足度100%!!</h3>
+										<p>大好評の企業研修。目的や期間に応じてオリジナルの研修プランをご提案いたします。</p>
+										<a class="arrow-link floatL" href="#">個人レッスンはこちら</a>
+									</div><div>
+									<img src="images/about06.png" /></div>
+								</div>
+							</div><div class="box-shadow feature">
+								<div class="grid grid-2x">
+									<div>
+										<h3>多彩なイベント開催で生徒さん同士仲良しに!!</h3>
+										<p>遠足やクリスマス会などのイベントは、生徒さん同士が交流を深め盛り上がっています。</p>
+									</div><div>
+									<img src="images/about07.png" /></div>
+								</div>
+							</div>
+					</div>
+
+					</div>
+
+				
+				</div>
+			</div>	
+
+
+			<div class="section group spacer">
+				<div class="content fontA group">
+					<div class="dtitle"><h2>東京パソコンアカデミーの実績</h2></div>
+					
+					<div class="box-border">
+							<h3>試験会場受賞履歴</h3>
+							<div>
+									<div>
+										2017年度
+									</div><div>
+										・都道府県別賞<br />　神奈川県2位<br />
+										・MOS世界学生大会入賞者輩出
+									</div>
+							</div>
+							<div>
+									<div>
+										2018年度
+									</div><div>
+										・OdysseyCBT賞<br />
+										・テストセンター特別賞<br />
+										・都道府県別賞<br />　神奈川県2位<br />　埼玉県2位
+										・MOS世界学生大会入賞者輩出
+									</div>
+							</div>
+							<a class="arrow-link floatL" href="#">MOS世界学生大会についてはこちら</a>
+					</div>
+			</div>
+
+				<div class="content fontA group">
+					<div class="dtitle"></div>
+					<div class="box-border">
+							<h3>職業訓練の履歴</h3>
+							<div>
+									<div>
+										埼玉県
+									</div><div>
+										公共職業訓練「518OAビジネス初級（9月）」平成22年9月〜11月 <br />Office2010（Word Excel PowerPoint）・就業支援・MOS対策
+									</div>
+							</div>
+							<div>
+									<div>
+										埼玉県
+									</div><div>
+										・公共職業訓練「533オフィスワーク初級（12月）」平成22年12月〜2月 <br />Office2010（Word Excel PowerPoint）・就業支援・MOS対策		
+									</div>
+							</div>
+							<div>
+									<div>
+										埼玉県
+									</div><div>
+										公共職業訓練「755オフィスワーク基礎（1月）（託児付）」平成23年1月〜3月 <br />Office2010（Word Excel PowerPoint）・就業支援・MOS対策
+									</div>
+							</div>
+							<a class="arrow-link floatL" href="#">講師派遣実績はこちら</a>
+					</div>
+
+			</div>
+
+
+
+			<?php include 'inc/news_information.php'; ?>
+
+
+
+			<div class="section section08 group" id="section08">
+				<div class="content content08 fontA group">
+					<div class="box620 group">
+						<h2><span>府中校へのお問い合わせはこちら</span></h2>
+						<p>お近くの教室で無料体験レッスンを受け付けてお<br class="sp-break" />ります。<br class="pc-break"/>ぜひお気軽にお申し込みください。</p>
+						<ul class="top-btn cta-btn group">
+							<li class="btn3"><a href="">無料体験レッスン</a></li>
+							<li class="btn2"><a href="">資料請求</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="section section09 group" id="section09">
+				<div class="content content09 fontA group">
+					<div class="box800 group">
+						<div class="desc2 desc09">
+							<h2>企業のみなさまへ</h2>
+							<p>企業向けパソコン研修、講師派遣、団体向け試験などのお申込み、<br class="pc-break"/>お問い合わせはこちらのフォームよりお待ちしております。</p>
+						</div>
+						<div class="btn-company">
+							<ul class="top-btn cta-btn group">
+								<li class="btn2"><a href="">法人向けのお問い合わせ</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+<?php include 'footer.php'; ?>
